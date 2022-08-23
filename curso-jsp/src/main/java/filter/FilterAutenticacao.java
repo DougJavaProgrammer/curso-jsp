77e8b3560345fb828d9a968b585839e23d44fc0e
Douglas Oliveira <dougmoroli@gmail.com>
2022-08-22 22:04:58 -0300

    criando o filter de login e autenticacao

---
+++ curso-jsp/src/main/java/filter/FilterAutenticacao.java
@@ -0,0 +1,62 @@
+package filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter(urlPatterns = { "/principal/*" }) /* Intercepta todas as requisições que vierem do projeto ou mapeamento */
+public class FilterAutenticacao implements Filter {
+
+	public FilterAutenticacao() {
+		super();
+	}
+
+	/* encerra os processos quando o servidor é parado */
+	/* mataria os processos de conexão com o banco */
+	public void destroy() {
+	}
+
+	/* intercepta as requisições e a as respostas no sistema */
+	/* tudo que fizemos no sistema, irá passar por aqui */
+	/* exemplo, validação de autenticação */
+	/* dar commit e rolback de transações no bancoo */
+	/* validar e fazer redirecionamento de páginas */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+
+		String usuarioLogado = (String) session.getAttribute("usuario");
+
+		String urlParaAutenticar = req.getServletPath(); /* url que está sendo acessada */
+
+		/* Validar se está logado, senão redireciona para a tela de login */
+		if (usuarioLogado == null
+				&& !urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) /* condição de não estar logado */ {
+			RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url= " + urlParaAutenticar);
+			request.setAttribute("msg", "Por favor, realize o login");
+			redireciona.forward(request, response);
+			return; /* para a execução e redireciona para o login */
+		} else {
+
+			chain.doFilter(request, response);
+
+		}
+
+	}
+
+	/* inicia os processos ou recursos quando o sevidor sobe o projeto */
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
--- curso-jsp/src/main/java/servlets/ServletLogin.java
+++ curso-jsp/src/main/java/servlets/ServletLogin.java
@@ -11,7 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
 
 /*O chamado Controller são as Servlets ou ServletLoginController*/
-@WebServlet("/ServletLogin") /* Mapeamento de URL que vem da tela */
+@WebServlet(urlPatterns = {"/principal/ServletLogin", "/ServletLogin"}) /* Mapeamento de URL que vem da tela */
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -31,31 +31,38 @@ public class ServletLogin extends HttpServlet {
 
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
-		
+		String url = request.getParameter("url");
+
 		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
 			ModelLogin modelLogin = new ModelLogin();
 			modelLogin.setLogin(login);
 			modelLogin.setSenha(senha);
-			
-			if(modelLogin.getLogin().equalsIgnoreCase("admin")) {/*Simulando o login*/
-				request.getSession().setAttribute("usuario", modelLogin.getLogin());//atributo de sessão
-				RequestDispatcher redirecionar = request.getRequestDispatcher("principal/principal.jsp");
+
+			if (modelLogin.getLogin().equalsIgnoreCase("admin")
+					&& modelLogin.getSenha().equalsIgnoreCase("admin")) {/* Simulando o login */
+				request.getSession().setAttribute("usuario", modelLogin.getLogin());// atributo de sessão
+
+				if (url == null || url.equals("null")) {
+					url = "principal/principal.jsp";
+
+				}
+
+				RequestDispatcher redirecionar = request.getRequestDispatcher(url);
 				redirecionar.forward(request, response);
-				
-				
-			}else {
-				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+
+			} else {
+				RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
 				request.setAttribute("msg", "Login ou senha inválida");
 				redirecionar.forward(request, response);
 			}
-								
-		}else {
+
+		} else {
 			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
 			request.setAttribute("msg", "Informe o login e senha");
 			redirecionar.forward(request, response);
-			
+
 		}
-						
+
 	}
 
 }
